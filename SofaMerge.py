1c789c0d9281942dddb8b9f2a000d0a5f1af55f4
Jacob Williams <jacobwilliams@users.noreply.github.com>
2015-06-07 16:11:22 -0500

    updated so it will work with any SOFA release.

--- SofaMerge.py
+++ SofaMerge.py
@@ -2,15 +2,30 @@
 """
 Combine all the SOFA source files into one module
 
-Example Usage: ./SofaMerge.py ~/sofa/20131202_d/f77/src/ > sofa.f
+Example Usage: 
 
-Tested with the 2013-12-02 release of SOFA.
+	# merge all the SOFA files:
+	python SofaMerge.py ./sofa/20150209_a/f77/src/ > sofa.f
+	
+	#
+	# for fixed-to-free conversion, could use: 
+	#   https://github.com/ylikx/fortran-legacy-tools
+	#
+	python fixed2free2.py sofa.f > sofa.f90
+	python flowercase.py sofa.f90 > sofa_module.f90
+
+Tested with the 2013 and 2015 releases of SOFA.
 http://www.iausofa.org/current_F.html
 
 """
 __appname__ = 'SofaMerge.py'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __author__  = 'Jacob Williams'
+
+import glob
+import time
+import sys
+import re
 
 ##########################################
 def rreplace(s, old, new, occurrence):
@@ -22,103 +37,11 @@ def rreplace(s, old, new, occurrence):
 #  Some text in the files:
 ##########################################
 
-_sofa_boilerplate="""
-*+----------------------------------------------------------------------
-*
-*  Copyright (C) 2013
-*  Standards Of Fundamental Astronomy Board
-*  of the International Astronomical Union.
-*
-*  =====================
-*  SOFA Software License
-*  =====================
-*
-*  NOTICE TO USER:
-*
-*  BY USING THIS SOFTWARE YOU ACCEPT THE FOLLOWING SIX TERMS AND
-*  CONDITIONS WHICH APPLY TO ITS USE.
-*
-*  1. The Software is owned by the IAU SOFA Board ("SOFA").
-*
-*  2. Permission is granted to anyone to use the SOFA software for any
-*     purpose, including commercial applications, free of charge and
-*     without payment of royalties, subject to the conditions and
-*     restrictions listed below.
-*
-*  3. You (the user) may copy and distribute SOFA source code to others,
-*     and use and adapt its code and algorithms in your own software,
-*     on a world-wide, royalty-free basis.  That portion of your
-*     distribution that does not consist of intact and unchanged copies
-*     of SOFA source code files is a "derived work" that must comply
-*     with the following requirements:
-*
-*     a) Your work shall be marked or carry a statement that it
-*        (i) uses routines and computations derived by you from
-*        software provided by SOFA under license to you; and
-*        (ii) does not itself constitute software provided by and/or
-*        endorsed by SOFA.
-*
-*     b) The source code of your derived work must contain descriptions
-*        of how the derived work is based upon, contains and/or differs
-*        from the original SOFA software.
-*
-*     c) The names of all routines in your derived work shall not
-*        include the prefix "iau" or "sofa" or trivial modifications
-*        thereof such as changes of case.
-*
-*     d) The origin of the SOFA components of your derived work must
-*        not be misrepresented;  you must not claim that you wrote the
-*        original software, nor file a patent application for SOFA
-*        software or algorithms embedded in the SOFA software.
-*
-*     e) These requirements must be reproduced intact in any source
-*        distribution and shall apply to anyone to whom you have
-*        granted a further right to modify the source code of your
-*        derived work.
-*
-*     Note that, as originally distributed, the SOFA software is
-*     intended to be a definitive implementation of the IAU standards,
-*     and consequently third-party modifications are discouraged.  All
-*     variations, no matter how minor, must be explicitly marked as
-*     such, as explained above.
-*
-*  4. You shall not cause the SOFA software to be brought into
-*     disrepute, either by misuse, or use for inappropriate tasks, or
-*     by inappropriate modification.
-*
-*  5. The SOFA software is provided "as is" and SOFA makes no warranty
-*     as to its use or performance.   SOFA does not and cannot warrant
-*     the performance or results which the user may obtain by using the
-*     SOFA software.  SOFA makes no warranties, express or implied, as
-*     to non-infringement of third party rights, merchantability, or
-*     fitness for any particular purpose.  In no event will SOFA be
-*     liable to the user for any consequential, incidental, or special
-*     damages, including any lost profits or lost savings, even if a
-*     SOFA representative has been advised of such damages, or for any
-*     claim by any third party.
-*
-*  6. The provision of any version of the SOFA software under the terms
-*     and conditions specified herein does not imply that future
-*     versions will also be made available under the same terms and
-*     conditions.
-*
-*  In any published work or commercial product which uses the SOFA
-*  software directly, acknowledgement (see www.iausofa.org) is
-*  appreciated.
-*
-*  Correspondence concerning SOFA software should be addressed as
-*  follows:
-*
-*      By email:  sofa@ukho.gov.uk
-*      By post:   IAU SOFA Center
-*                 HM Nautical Almanac Office
-*                 UK Hydrographic Office
-*                 Admiralty Way, Taunton
-*                 Somerset, TA1 2DN
-*                 United Kingdom
-*
-*-----------------------------------------------------------------------
-"""
+#the SOFA boilerplate text:
+_sofa_boilerplate_start = '\\*\\+----------------------------------------------------------------------'
+_sofa_boilerplate_end = '\\*-----------------------------------------------------------------------\n'
+_sofa_boilerplate_re = '('+_sofa_boilerplate_start+')(.*?)('+_sofa_boilerplate_end+')'
+_sofa_boilerplate_rg = re.compile(_sofa_boilerplate_re,re.IGNORECASE|re.DOTALL)
 
 _sofa_finished = """*  Finished.
 
@@ -131,11 +54,6 @@ _sofa_divider = """* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ##########################################
 def cleansofa(srcdir):
 	'''main routine : process all the files.'''
-
-	import glob
-	import time
-	import sys
-	import re
 	
 	module_name = "sofa_module"	
 	div = "***********************************************************************"
@@ -163,6 +81,7 @@ def cleansofa(srcdir):
 	html_file_list = sorted(glob.glob(srcdir+"*.for"))
 
 	module_content = ''
+	get_boilerplate = True
 	
 	#loop through each file:
 	for f in html_file_list:
@@ -200,8 +119,19 @@ def cleansofa(srcdir):
 					print('Error parsing file: '+name)
 					sys.exit(0)
 
+				#save the boilerplate, so it can be added to the end of the file:
+				# [only need to do this once]
+				if (get_boilerplate):
+					m = _sofa_boilerplate_rg.search(data)
+					if m:
+						c1=m.group(1)
+						c2=m.group(2)
+						c3=m.group(3)
+						_sofa_boilerplate = c1+c2+c3
+						get_boilerplate = False
+				
 				#remove some unwanted text:
-				data = data.replace(_sofa_boilerplate,'')
+				data = re.sub(_sofa_boilerplate_re,'',data, flags=re.DOTALL)
 				data = data.replace(_sofa_finished,'')
 				data = data.replace(_sofa_divider,'')
 								
@@ -231,9 +161,7 @@ def cleansofa(srcdir):
 				line = line[0:m.span()[0]]+m.group(1)+m.group(3)+line[m.span()[1]:]
 			print(line)
 	##########
-			
-	# Could also include some fixed-to-free conversions here
-		
+				
 	#finished with all the files:
 	print(c+div)
 	print("      end module "+module_name)
